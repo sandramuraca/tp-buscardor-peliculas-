da4fc90f844041a8aad7dd5387371a9e3299ee6a
Sandra Muraca <sandramuraca78@gmail.com>
2022-02-25 22:13:21 -0300

    stilo mas o menos barra navegacion

--- src/componentes/Nav.js
+++ src/componentes/Nav.js
@@ -1,15 +1,57 @@
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 const Nav = () =>{
     return (
       <div>
-        Esto es el Nav
+        
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar 
+          position="fixed"
+          sx={{ bgcolor: "red"}}>
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+              <MenuIcon />
+              </IconButton>
+              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                Buscador Películas
+              </Typography>
+              <Button color="inherit">
+                <Link to="/"> Home </Link>
+              </Button>
+                 
+              <Button>
+                <Link to="/ultimos-lanzamientos">Ultimos Lanzamientos</Link>
+              </Button> 
+
+              <Button>
+                <Link to="/populares">Populares </Link>
+              </Button>
+
+              <Button>
+              <Link to="/buscar">Buscar</Link>
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
         
         <ul>
-            <li> <Link to="/"> Home </Link> </li>
-            <li> <Link to="/ultimos-lanzamientos">Ultimos Lanzamientos</Link></li>
-            <li><Link to="/populares">Populares </Link></li>
-            <li> <Link to="/buscar">Buscar</Link></li>
+            
+            <li></li>
+            <li> </li>
         </ul>
 
       </div>
