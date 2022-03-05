bfd1cc8e252613f9105c6ce577c3bd2b71666d55
Sandra Muraca <sandramuraca78@gmail.com>
2022-03-04 21:54:43 -0300

    haciendo fetch a la api para armar los componentes de los listados

--- src/componentes/Listados.js
+++ src/componentes/Listados.js
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
+import DetallePelicula from "./DetallePelicula"
+import { Box } from "@mui/system";
 
-const Listados = () =>{
+const Listados = ({titulo, url}) =>{
+
+  const [peliculas, setPeliculas] = useState([])
+  useEffect (() => {
+    fetch(`https://api.themoviedb.org/3/movie/${url}?api_key=1e2f4ae9cf56f3e1c8367fb458677972&language=es-AR&page=1`)
+    .then(res => res.json())
+    .then(data => setPeliculas(data.results))
+  }, [])
     return (
-      <div>
-        <h3>Aca van los listados</h3>
-        
-      </div>
+      
+      <Box>
+        <h2>{titulo}</h2>
+        {peliculas.map(pelicula => (
+          <DetallePelicula
+          titulo={pelicula.title}
+          imagen={`https://image.tmdb.org/t/p/w300/${pelicula.poster_path}`}
+          link={`/${peliculas.id}`}
+          />
+        ) )}
+      </Box>
   
     )
   }
