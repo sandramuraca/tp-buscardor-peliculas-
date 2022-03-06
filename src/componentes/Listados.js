fd3537e23694c02e37571a60eb92198546686352
Sandra Muraca <sandramuraca78@gmail.com>
2022-03-05 23:59:56 -0300

    items listados peliculas y estilo listados

--- src/componentes/Listados.js
+++ src/componentes/Listados.js
@@ -1,25 +1,30 @@
 import { useState, useEffect } from "react";
-import DetallePelicula from "./DetallePelicula"
 import { Box } from "@mui/system";
+import ItemListadoPelicula from './ItemListadoPelicula';
+import Typography from '@mui/material/Typography';
 
-const Listados = ({titulo, url}) =>{
+const Listados = ({titulo, urlListado}) =>{
 
   const [peliculas, setPeliculas] = useState([])
   useEffect (() => {
-    fetch(`https://api.themoviedb.org/3/movie/${url}?api_key=1e2f4ae9cf56f3e1c8367fb458677972&language=es-AR&page=1`)
+    fetch(`https://api.themoviedb.org/3/movie/${urlListado}?api_key=1e2f4ae9cf56f3e1c8367fb458677972&language=es-AR&page=1`)
     .then(res => res.json())
     .then(data => setPeliculas(data.results))
   }, [])
     return (
-      
       <Box>
-        <h2>{titulo}</h2>
+        <Typography sx={{ color: "#5865f2"}} 
+        variant="h4" 
+        gutterBottom component="div">
+        {titulo}
+        </Typography>
+        <h2></h2>
         {peliculas.map(pelicula => (
-          <DetallePelicula
+          <ItemListadoPelicula 
+          key={pelicula.id}
           titulo={pelicula.title}
           imagen={`https://image.tmdb.org/t/p/w300/${pelicula.poster_path}`}
-          link={`/${peliculas.id}`}
-          />
+          link={`/detallePelicula/${peliculas.id}`}/>
         ) )}
       </Box>
   
