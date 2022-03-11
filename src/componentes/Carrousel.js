b56cbdc455b4b9d1f1457492d916b71ec2164bf0
Sandra Muraca <sandramuraca78@gmail.com>
2022-03-10 21:43:29 -0300

    importo libreria carrousel y primer prueba

--- src/componentes/Carrousel.js
+++ src/componentes/Carrousel.js
@@ -1,9 +1,42 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
+ 
 const Carrousel = () => {
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+    },
+    {
+      name: "Random Name #2",
+      description: "hello world",
+    },
+    {
+      name: "Random Name #3",
+      description: "lalala",
+    },
+  ];
+ 
   return (
-    <div>
-      <h3>Aca va el Carrousel</h3>
-    </div>
+    <Carousel>
+      {items.map((item, i) => (
+        <Item key={i} item={item} />
+      ))}
+    </Carousel>
   );
 };
-
+ 
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+ 
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
+}
+ 
 export default Carrousel;
+ 
