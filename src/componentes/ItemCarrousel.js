27e2ac020c4d56de9865a3d274a2afb494282ae5
Sandra Muraca <sandramuraca78@gmail.com>
2022-03-17 20:25:15 -0300

    texto descripcion ok

--- src/componentes/ItemCarrousel.js
+++ src/componentes/ItemCarrousel.js
@@ -1,32 +1,46 @@
 import { Box } from "@mui/system";
 import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
-const ItemCarrousel = ({tituloSlider, imagenSlider, descripcionSlider})=>{
-    return (
-      
-          <Box
-            sx={{backgroundImage:`url(${imagenSlider})`,
-            backgroundRepeat: "no-repeat",
-            width: "100%",
-            height: "500px",
-            backgroundSize: "cover",
-            backgroundPosition: "50% 0%",
-            zIndex: "-20"            
-            }}>
-              <Box
-              sx={{
-                display:"flex", 
-                flexDirection:"column",
-                alignItems:"center",
-              }}>
-                <h2>{tituloSlider}</h2>
-                <p>{descripcionSlider}</p>
-                <Button>Ver más</Button>
-              </Box>
-             </Box>
-          
-           
+const ItemCarrousel = ({ tituloSlider, imagenSlider, descripcionSlider }) => {
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url(${imagenSlider})`,
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "500px",
+        backgroundSize: "cover",
+        backgroundPosition: "50% 0%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "600px",
+          bgcolor: "#cacaca",
+          opacity: [0.9, 0.8, 0.7],
+          borderRadius: "5px",
+        }}
+      >
+        <Typography variant="h5" gutterBottom component="div" p={2}>
+          {tituloSlider}
+        </Typography>
+        <Typography variant="body1" gutterBottom p={2}>
+          {descripcionSlider}
+        </Typography>
+        <p></p>
+        <Button>Ver más</Button>
+      </Box>
+    </Box>
   );
-}
+};
 
-export default ItemCarrousel
+export default ItemCarrousel;
