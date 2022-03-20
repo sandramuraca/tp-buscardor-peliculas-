895fbd9ec2c6e960383435df755c68ecf087702b
Sandra Muraca <sandramuraca78@gmail.com>
2022-03-19 22:10:31 -0300

    cambios en paginador

--- src/componentes/Populares.js
+++ src/componentes/Populares.js
@@ -1,21 +1,31 @@
 import { Box } from "@mui/material";
 import EstructuraPeliculasNuevasYPopulares from "./EstructuraPeliculasNuevasYPolulares";
 import { useState, useEffect } from "react";
-import { baseUrl, apiKey } from "../auxiliares/Auxiliares";
+import { baseUrl, apiKey, paginaActual } from "../auxiliares/Auxiliares";
 
 const Populares = () => {
   const [peliculas, setPeliculas] = useState([]);
+  const [pagina, setPagina] = useState(1);
+
   useEffect(() => {
-    fetch(`${baseUrl}popular?api_key=${apiKey}&language=es-AR&page=1`)
+    fetch(`${baseUrl}popular?api_key=${apiKey}&language=es-AR&page=${paginaActual}`)
       .then((res) => res.json())
       .then((data) => setPeliculas(data.results));
-  }, []);
+  }, [pagina]);
+  const handleClickSiguiente =() =>{
+    setPagina(paginaActual++)
+  }
 
+  const handleClickAnterior = () =>{
+    setPagina(paginaActual--)
+  }
   return (
     <Box sx={{ marginTop: 10 }}>
       <EstructuraPeliculasNuevasYPopulares
         tituloSeccion="Películas Populares"
         peliculas={peliculas}
+        clickBotonPagSiguiente={handleClickSiguiente}
+        clickBotonPagAnterior={handleClickAnterior}
       />
     </Box>
   );
--- src/componentes/UltimosLanzamientos.js
+++ src/componentes/UltimosLanzamientos.js
@@ -1,21 +1,33 @@
 import { Box } from "@mui/system";
 import EstructuraPeliculasNuevasYPopulares from "./EstructuraPeliculasNuevasYPolulares";
 import { useState, useEffect } from "react";
-import { baseUrl, apiKey } from "../auxiliares/Auxiliares";
+import { baseUrl, apiKey, paginaActual } from "../auxiliares/Auxiliares";
 
 const UltimosLanzamientos = () => {
   const [peliculas, setPeliculas] = useState([]);
+  const [pagina, setPagina] = useState(1);
+
   useEffect(() => {
-    fetch(`${baseUrl}now_playing?api_key=${apiKey}&language=es-AR&page=1`)
+    fetch(`${baseUrl}now_playing?api_key=${apiKey}&language=es-AR&page=${paginaActual}`)
       .then((res) => res.json())
       .then((data) => setPeliculas(data.results));
-  }, []);
+  }, [pagina]);
 
+  const handleClickSiguiente =() =>{
+    setPagina(paginaActual++) 
+  }
+
+  const handleClickAnterior = () =>{
+    setPagina(paginaActual--) 
+  }
+  
   return (
     <Box sx={{ marginTop: 10 }}>
       <EstructuraPeliculasNuevasYPopulares 
         tituloSeccion="Ultimos Lanzamientos"
         peliculas={peliculas}
+        clickBotonPagSiguiente={handleClickSiguiente}
+        clickBotonPagAnterior={handleClickAnterior}
       />
     </Box>
   );
